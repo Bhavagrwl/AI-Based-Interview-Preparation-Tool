9c554c6e99c5fb9a8e850a54181e6a6edb483b6a
Bhavya Agrawal <bhavya.agrawal1107@gmail.com>
2026-04-03 20:38:52 +0530

    vercel setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,30 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/config/database");
+const serverless = require("serverless-http");
 
-connectToDB();
+// connectToDB();
 
-app.listen(3000, () => {
-  console.log("Server on 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server on 3000");
+// });
+
+// app.listen(3000, "0.0.0.0", () => {
+//   console.log("Server running");
+// });
+
+let isConnected = false;
+
+const connectDB = async () => {
+  if (!isConnected) {
+    await connectToDB();
+    isConnected = true;
+  }
+};
+
+const handler = serverless(app);
+
+module.exports = async (req, res) => {
+  await connectDB();
+  return handler(req, res);
+};
